e1fe44fc573c025c274f14c3f5df471b9733490b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-20 12:16:10 -0700

    human number

--- crawler/go.mod
+++ crawler/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.18 // indirect
 	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly/v2 v2.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
--- crawler/main.go
+++ crawler/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/gocolly/colly/v2"
 )
 
@@ -23,10 +25,17 @@ var blocked = []string{
 	"/roadmap",
 }
 
+type Repo struct {
+	url  string
+	star int64
+}
+
 func main() {
 	_ = os.Mkdir(".cache", os.ModePerm)
 
 	c := colly.NewCollector(colly.AllowedDomains("github.com"), colly.CacheDir((".cache")))
+
+	var repos []Repo
 
 	// Find and visit all links
 	c.OnHTML("a[data-hovercard-type=repository]", func(e *colly.HTMLElement) {
@@ -62,6 +71,13 @@ func main() {
 
 	c.OnHTML("#repo-stars-counter-star", func(e *colly.HTMLElement) {
 		fmt.Println(e.Request.URL, ": Star = ", e.DOM.Text())
+
+		star, _, _ := humanize.ParseSI(e.DOM.Text())
+
+		repos = append(repos, Repo{
+			url:  e.Request.URL.String(),
+			star: int64(star),
+		})
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -70,4 +86,17 @@ func main() {
 
 	c.Visit("https://github.com/swc-project/swc/network/dependents?dependent_type=PACKAGE&package_id=UGFja2FnZS00Njc1MTk4MDQ%3D")
 	// c.Visit("https://github.com/parcel-bundler/parcel/network/dependents?dependent_type=REPOSITORY&package_id=UGFja2FnZS0yOTE0Mjg3MjM4")
+
+	sort.Sort(sort.Reverse(ByStar(repos)))
+
+	for _, repo := range repos {
+		fmt.Println(repo.url, repo.star)
+	}
+
 }
+
+type ByStar []Repo
+
+func (a ByStar) Len() int           { return len(a) }
+func (a ByStar) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByStar) Less(i, j int) bool { return a[i].star < a[j].star }
