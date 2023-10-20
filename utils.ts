e71032bc6553dea556a83100d569601865a238e2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-20 12:24:52 -0700

    install @swc/jest

--- utils.ts
+++ utils.ts
@@ -466,5 +466,5 @@ export async function installSwc({ version }: { version: string }) {
   await fs.promises.mkdir(swcPath, { recursive: true });
   await fs.promises.writeFile(path.join(swcPath, "package.json"), "{}", "utf8");
   cd(swcPath);
-  await $`npm install @swc/core@${version} @swc/types ts-node@11.0.0-beta.1 --no-save --force`;
+  await $`npm install @swc/core@${version} @swc/jest @swc/types ts-node@11.0.0-beta.1 --no-save --force`;
 }
