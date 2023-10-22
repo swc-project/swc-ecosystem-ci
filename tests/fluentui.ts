852772ff731b655bf136376761ab33f48e2bbf12
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-21 23:05:58 -0700

    fluentui

---
+++ tests/fluentui.ts
@@ -0,0 +1,12 @@
+import { runInRepo } from "../utils";
+import { RunOptions } from "../types";
+
+export async function test(options: RunOptions) {
+  await runInRepo({
+    ...options,
+    repo: "microsoft/fluentui",
+    branch: "master",
+    build: "build",
+    test: ["test"],
+  });
+}
