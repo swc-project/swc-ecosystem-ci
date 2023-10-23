2457ed181f1244389e610e6a3f5f0e3d14a4f3a2
Alex Yang <himself65@outlook.com>
2023-10-23 18:52:46 -0500

    feat: add waku

---
+++ tests/waku.ts
@@ -0,0 +1,12 @@
+import { runInRepo } from "../utils";
+import { RunOptions } from "../types";
+
+export async function test(options: RunOptions) {
+  await runInRepo({
+    ...options,
+    repo: "dai-shi/waku",
+    branch: "main",
+    build: "compile",
+    test: "e2e",
+  });
+}
